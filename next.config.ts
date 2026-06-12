ddf3ec35f5b8f1b90bf4151c1ed3133cc195aeba
Aman Yadav <amanyadav16052007@gmail.com>
2026-06-13 01:45:34 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,41 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    allowedDevOrigins: [
+      '9000-firebase-studio-1781187803414.cluster-ulqnojp5endvgve6krhe7klaws.cloudworkstations.dev',
+      '*.cloudworkstations.dev',
+    ],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
